ee81a67c8752f6161618482ea039d0f69f3678ac
DeneaRansom <webb.denea@gmail.com>
2021-04-23 17:42:47 -0400

    user register & validate session

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const { validateSession } = require('../middleware');
 const { UserModel } = require("../models");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const { UniqueConstraintError } = require("sequelize/lib/errors")
 
 router.get('/test', (req,res) => {
     res.send("This is my user test route.")
@@ -12,31 +16,82 @@ router.post('/register', async (req,res) => {
         const newUser = await UserModel.create({
             email,
             username,
-            password,
             firstName,
-            lastName
+            lastName,
+            password: bcrypt.hashSync(password, 13)
         });
+
+        const token = jwt.sign(
+            {id: newUser.id},
+            process.env.JWT_SECRET,
+            {
+                expiresIn: 60 * 60 * 24
+            }
+
+        )
 
         //console.log("new user", newUser, email, username);
         res.status(201).json({
             message: "User Registered!",
-            user: newUser
-        })
+            user: newUser,
+            token
+        }) 
     } catch(err) {
+      if(err instanceof UniqueConstraintError) {
+          res.status(409).json({
+              message: "Email already in use." //Username already in use
+          })
+      } else {
         res.status(500).json({
             message: "Failed to register",
             error: err
         })
+    
+    } 
     }
-
 })
 
-router.post('/login', (req,res) => {
-    res.send("You logged in REAL nice");
+router.post('/login', async (req,res) => {
+    let {username, password} = req.body;
+    try {
+        let loginUser = await UserModel.findOne({
+            where: {
+                username
+            }
+        })
+        if (loginUser) {
+            let passwordComparison = await bcrypt.compare(password, loginUser.password); //need to add bcrypt
+            
+             if (passwordComparison) {
+                 const token = jwt.sign(
+                     {id: loginUser.id}, 
+                     process.env.JWT_SECRET, 
+                     {expiresIn: 60 * 60 * 24}
+                     )
+                 
+                 res.status(200).json({
+                     message: "User successfully logged in!",
+                     user: loginUser,
+                     token
+                 })
+             } else {
+                 res.status(401).json({
+                     message: "Error: password."
+                 })
+             }
+        } else {
+            res.status(401).json({
+                message: "Error: username."
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: `Failed to login user. ${err}`
+        })
+    }
 })
+
 
 
 
 module.exports = router;
-
-//Testing-Denea!
