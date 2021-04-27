66a39578031f2baa4a480881cfc7d425ad91bddc
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-04-27 13:08:42 -0400

    heroku push

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { validateSession } = require('../middleware');
 const { UserModel } = require("../models");
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
