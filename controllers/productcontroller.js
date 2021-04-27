e17b2b769a89f6a5dee279fd05d856a579723099
jgpowellcreates <79958558+jgpowellcreates@users.noreply.github.com>
2021-04-27 10:47:44 -0400

    Update productcontroller.js
    Replaced user code with proper product code.

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -1,159 +1,76 @@
 const router = require('express').Router();
-const { validateSession } = require('../middleware');
-const { UserModel } = require("../models");
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
-const { UniqueConstraintError } = require("sequelize/lib/errors")
+const { ProductModel } = require("../models");
 
-router.get('/test', (req, res) => {
-    res.send("This is my user test route.")
-})
-
-/*
-============================================================
-/user/regiser POST (Create a New User)
-============================================================
-*/
-
-router.post('/register', async (req, res) => {
-    const { email, username, password, firstName, lastName } = req.body; //revisit when we have modals
-    try {
-        const newUser = await UserModel.create({
-            email,
-            username,
-            firstName,
-            lastName,
-            password: bcrypt.hashSync(password, 13),
-            shoppingCartContents: []
-        });
-
-        const token = jwt.sign(
-            { id: newUser.id },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: 60 * 60 * 24
-            }
-        )
-
-        //console.log("new user", newUser, email, username);
-        res.status(201).json({
-            message: "User Registered!",
-            user: newUser,
-            token
-        });
-    } catch (err) {
-        if (err instanceof UniqueConstraintError) {
-            res.status(409).json({
-                message: "Email/Username already in use." //Username already in use
-            })
-        } else {
-            res.status(500).json({
-                message: "Failed to register",
-                error: err,
-                messageOrigin: "userController.js"
-            })
-
-        }
-    }
-})
-
-/*
-============================================================
-/user/login POST (Find an Existing User)
-============================================================
-*/
-
-router.post('/login', async (req, res) => {
-    let { username, password } = req.body;
-    try {
-        let loginUser = await UserModel.findOne({
-            where: {
-                username
-            }
-        })
-        if (loginUser) {
-            let passwordComparison = await bcrypt.compare(password, loginUser.password); //need to add bcrypt
-
-            if (passwordComparison) {
-                const token = jwt.sign(
-                    { id: loginUser.id },
-                    process.env.JWT_SECRET,
-                    { expiresIn: 60 * 60 * 24 }
-                )
-
-                res.status(200).json({
-                    message: "User successfully logged in!",
-                    user: loginUser,
-                    token
-                })
-            } else {
-                res.status(401).json({
-                    message: "Error: password."
-                })
-            }
-        } else {
-            res.status(401).json({
-                message: "Error: username."
-            })
-        }
-    } catch (err) {
-        res.status(500).json({
-            error: `Failed to login user. ${err}`
-        })
-    }
-})
-
-/*
-============================================================
-/user/update PUT (Update an existing user)
-============================================================
-*/
-
-router.put("/update/:userId", async (req, res) => {
-
-    const { username, email, firstName, lastName } = req.body;
-    const { userId } = req.params;
-
-    const query = {
-        where: {
-            id: userId
-        }
-    };
-
-    const updatedUser = {
-        username,
-        email,
-        firstName,
-        lastName
-    };
-
-    try {
-        const update = await UserModel.update(updatedUser, query);
-        res.status(200).json(updatedUser);
-    } catch (err) {
-        res.status(500).json({ error: err })
-    }
+router.get("/test", (req,res) => {
+    res.send("This is my product test route");
 });
 
 /*
 ============================================================
-/user/delete/:ownerId DELETE (Delete an Existing User)
+/product/create POST (This creates a new product)
+============================================================
+*/
+//When adding a new product to your store, button must pass in Store Id (to associate product)
+
+router.post('/create/:storeId', async (req,res) => {
+    const {productName, price, description, stock} = req.body;
+
+    const { storeId } = req.params;
+    try{
+        const newProduct = await ProductModel.create({
+            productName,
+            price,
+            description,
+            stock,
+            imageURL: "https://static01.nyt.com/images/2018/05/03/us/03spongebob_xp/03spongebob_xp-videoSixteenByNineJumbo1600.jpg",
+            storeId
+        });
+
+        res.status(201).json({
+            message: "Product Created!",
+            user: newProduct
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: "Failed to create product",
+            error: err
+        })
+    }
+})
+
+/*
+============================================================
+/product/ GET (This returns all the products)
 ============================================================
 */
 
-router.delete("/delete/:ownerId", async (req, res) => {
-
-    const { ownerId } = req.params;
+router.get('/', async (req, res) => {
 
     try {
-        const query = {
-            where: {
-                id: ownerId
-            }
-        };
+        const products = await ProductModel.findAll();
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
 
-        await UserModel.destroy(query);
-        res.status(200).json({ message: "User Removed" });
+
+/*
+============================================================
+/product/:id GET (This displays a specific product)
+============================================================
+*/
+
+router.get('/:productID', async (req, res) => {
+    let { productID } = req.params;
+
+    try {
+        const product = await ProductModel.findAll({
+            where: {
+                id: productID
+            }
+        });
+        res.status(200).json(product);
     } catch (err) {
         res.status(500).json({ error: err });
     }
@@ -161,69 +78,60 @@ router.delete("/delete/:ownerId", async (req, res) => {
 
 /*
 ============================================================
-/user/addtocart/:userID POST (Adds a productID to the shoppingcartArray of a User)
+/product/:id PUT (This updates a specific product)
 ============================================================
 */
 
-router.put("/addtocart/:userID", async (req, res) => {
-    const { userID } = req.params;
-    const { productID } = req.body;
-    
-    let currentUser = await UserModel.findOne({
-        where: {
-            id: userID
-        }
-    })
-
-    const currentArray = currentUser.shoppingCartContents;
-
-    currentArray.push(productID);
-
-    console.log(currentArray)
+router.put('/:productID', async (req, res) => {
+    const {productName, price, description, stock} = req.body;
+    let { productID } = req.params;
+    // let { userID } = req.user;
 
     const query = {
         where: {
-            id: userID
+            id: productID
+            // storeOwner : userID
         }
     };
 
-    const updatedUser = {
-        shoppingCartContents: currentArray
-    };
+    const newProduct = {
+        productName,
+        price,
+        description,
+        stock
+    }
 
     try {
-        const update = await UserModel.update(updatedUser, query);
-        res.status(200).json(updatedUser);
+        const update = await ProductModel.update(newProduct, query);
+        res.status(200).json(newProduct);
     } catch (err) {
-        res.status(500).json({ error: err })
+        res.status(500).json({ error: err });
     }
-});
+})
 
 /*
 ============================================================
-/user/checkout/:userID POST (Adds a productID to the shoppingcartArray of a User)
+/product/:id DELETE (This deletes a specific product)
 ============================================================
 */
 
-router.put("/checkout/:userID", async (req, res) => {
-    const { userID } = req.params;
-
-    const query = {
-        where: {
-            id: userID
-        }
-    };
-
-    const updatedUser = {
-        shoppingCartContents: []
-    };
+router.delete('/:productID', async (req, res) => {
+    let { productID } = req.params;
+    // { userID } = req.user
 
     try {
-        const update = await UserModel.update(updatedUser, query);
-        res.status(200).json(updatedUser);
+        const query = {
+            where: {
+                id: productID
+                // storeOwner : userID
+            }
+        };
+
+        await ProductModel.destroy(query);
+        res.status(200).json({message: "Product deleted"});
     } catch (err) {
-        res.status(500).json({ error: err })
+        res.status(500).json({error: err});
     }
-});
+})
 
 module.exports = router;
